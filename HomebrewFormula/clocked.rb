f3f7b590890b6e04354ea38f1c97ea483a334d96
Horst Gutmann <horst@zerokspot.com>
2017-10-28 15:07:26 +0200

    Clocked v1.1.0

--- HomebrewFormula/clocked.rb
+++ HomebrewFormula/clocked.rb
@@ -1,9 +1,9 @@
 class Clocked < Formula
   desc "A simple time-tracker which can be synchronized with JIRA"
   homepage "https://github.com/zerok/clocked"
-  url "https://github.com/zerok/clocked/releases/download/v1.0.0/clocked_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "fce22a04e1b4fc93d2fa224e91bf555b20a7afa56d7f454ee80c617f70e06297"
+  url "https://github.com/zerok/clocked/releases/download/v1.1.0/clocked_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "ebea663f94bcc1ae375b861a34190af745f94784a35361270bf550bd57c3b376"
 
   def install
     bin.install "clocked"
