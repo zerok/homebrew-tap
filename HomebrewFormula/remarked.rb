f69417dd54d7daa1a0a088b9c99ea60c5c05dd26
Horst Gutmann <zerok@zerokspot.com>
2018-07-19 19:51:06 +0200

    remarked v1.3.0

--- HomebrewFormula/remarked.rb
+++ HomebrewFormula/remarked.rb
@@ -1,9 +1,9 @@
 class Remarked < Formula
     desc "A simple wrapper around Remark.JS that offers a guided mode"
     homepage "https://github.com/zerok/remarked"
-    url "https://github.com/zerok/remarked/releases/download/v1.2.1/remarked_1.2.1_darwin_amd64.tar.gz"
-    version "1.2.1"
-    sha256 "0314ed2e1d6b0a1457cfdf103e76b21cad03ccf853dcdffc271c1b207b282c67"
+    url "https://github.com/zerok/remarked/releases/download/v1.3.0/remarked_1.3.0_darwin_amd64.tar.gz"
+    version "1.3.0"
+    sha256 "766dbb21c7fc61c12eaf0af6c011d16f821034bd697b1f5b0384755382e4dd81"
   
     def install
       bin.install "remarked"
