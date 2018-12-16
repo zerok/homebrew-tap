58543b49636cf572a1f458a59d416c4c12c419fb
Horst Gutmann <zerok@zerokspot.com>
2018-12-16 17:43:51 +0100

    container-inventory v1.2.0

--- HomebrewFormula/container-inventory.rb
+++ HomebrewFormula/container-inventory.rb
@@ -1,9 +1,9 @@
 class ContainerInventory < Formula
   desc "A CLI for working with container registries"
   homepage "https://gitlab.com/zerok/container-inventory"
-  url "https://s3.eu-central-1.amazonaws.com/h10n-packages/container-inventory/1.1.0/container-inventory_1.1.0_macos_x86_64.tar.gz"
-  version "1.1.0"
-  sha256 "c83e322c647ad859ce034a77d407cf7661f89952c0f7731e5673f8a68367cf47"
+  url "https://s3.eu-central-1.amazonaws.com/h10n-packages/container-inventory/1.2.0/container-inventory_1.2.0_macos_x86_64.tar.gz"
+  version "1.2.0"
+  sha256 "4f89ad7088ab42785db371b4380fc1b97a2cec1f2aab678e5390b5297589024b"
 
   def install
     bin.install "container-inventory"
