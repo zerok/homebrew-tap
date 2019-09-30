95f84358fcd572320fd580cb5065d4e5a8570039
Horst Gutmann <zerok@zerokspot.com>
2019-09-30 13:37:27 +0200

    tpl v2.6.0

--- HomebrewFormula/tpl.rb
+++ HomebrewFormula/tpl.rb
@@ -1,9 +1,9 @@
 class Tpl < Formula
   desc "A simple templating solution for configuration files"
   homepage "https://github.com/zerok/tpl"
-  url "https://github.com/zerok/tpl/releases/download/v2.5.1/tpl_2.5.1_darwin_amd64.tar.gz"
-  version "2.5.1"
-  sha256 "60d1fc2eebec5fb3fe7177c18951ee4eb830656c8246fa42191b90c25d6ec879"
+  url "https://github.com/zerok/tpl/releases/download/v2.6.0/tpl_2.6.0_darwin_amd64.tar.gz"
+  version "2.6.0"
+  sha256 "3c3d726b6acddbd92bb4577e89ea5f912aa01472c5ddbc71193d702b254a84b5"
 
   def install
     bin.install "tpl"
